db5b69d415764e5b4d1f0fd7a392f9a17682f3ea
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-07-17 23:37:29 +0000

    create movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,27 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    @movie_title = params.fetch("title")
+    @movie_year = params.fetch("year")
+    @movie_duration = params.fetch("duration")
+    @movie_description = params.fetch("description")
+    @movie_image = params.fetch("image")
+    @movie_director_id = params.fetch("director_id")
+
+    x = Movie.new
+    x.title = @movie_title
+    x.year = @movie_year
+    x.duration = @movie_duration
+    x.description = @movie_description
+    x.image = @movie_image
+    x.director_id = @movie_director_id
+    x.save
+    
+    matching_movies = Movie.all
+    @list_of_movies = matching_movies.order({ :created_at => :desc })
+
+    render({ :template => "movie_templates/index" })
+  end
 end
