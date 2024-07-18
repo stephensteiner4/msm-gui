b6d9480ef297355e9c400672282d954d0eb0d8f0
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-07-18 01:12:49 +0000

    finalize and submit assignment

--- config/routes.rb
+++ config/routes.rb
@@ -7,19 +7,19 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
   post("/directors", { :controller => "directors", :action => "create"})
-  get("/directors/:path_id/delete", { :controller => "directors", :action => "destroy" })
+  get("/delete_director/:path_id", { :controller => "directors", :action => "destroy" })
   post("/directors/:path_id/modify", { :controller => "directors", :action => "modify" })
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   post("/movies", { :controller => "movies", :action => "create"})
-  get("/movies/:path_id/delete", { :controller => "movies", :action => "destroy" })
+  get("/delete_movie/:path_id", { :controller => "movies", :action => "destroy" })
   post("/movies/:path_id/modify", { :controller => "movies", :action => "modify" })
 
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
   post("/actors", { :controller => "actors", :action => "create" })
-  get("/actors/:path_id/delete", { :controller => "actors", :action => "destroy" })
+  get("/delete_actor/:path_id", { :controller => "actors", :action => "destroy" })
   post("/actors/:path_id/modify", { :controller => "actors", :action => "modify" })
 end
